57a4722165354416707c3103758cbe12e98e2d7a
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2023-01-20 18:28:19 +0100

    add sdde scheme

--- nfjax/__init__.py
+++ nfjax/__init__.py
@@ -1,3 +1,3 @@
-from .loops import make_sde, make_ode, make_dde
+from .loops import make_sde, make_ode, make_dde, make_sdde
 from .shtlc import make_shtdiff
 from .neural_mass import JRState, JRTheta, jr_dfun, jr_default_theta
