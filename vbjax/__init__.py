9d7903c90942c074d9c41f8c3b73535e0575efbf
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2023-02-09 17:20:59 +0100

    add test for difference coupling

--- vbjax/__init__.py
+++ vbjax/__init__.py
@@ -2,4 +2,5 @@ from .loops import make_sde, make_ode, make_dde, make_sdde
 from .shtlc import make_shtdiff
 from .neural_mass import JRState, JRTheta, jr_dfun, jr_default_theta
 from .regmap import make_region_mapping
+from .coupling import make_diff_cfun, make_linear_cfun
 from ._version import __version__
