5147db43534ffe525b0ceab01d16cbe023f797c4
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2023-02-09 17:20:59 +0100

    add test for difference coupling

--- vbjax/tests/__main__.py
+++ vbjax/tests/__main__.py
@@ -1,6 +1,6 @@
-from . import test_loops, test_regmap, test_shtlc
+from . import test_loops, test_regmap, test_shtlc, test_coupling
 
-test_mods = [test_loops, test_regmap, test_shtlc]
+test_mods = [test_loops, test_regmap, test_shtlc, test_coupling]
 
 for mod in test_mods:
     for key in dir(mod):
