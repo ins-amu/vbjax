3197a941ab998288b00d4522e51a12cfa7868b8a
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2023-04-06 10:20:12 +0200

    ensure montbrio is available by default

--- vbjax/__init__.py
+++ vbjax/__init__.py
@@ -1,6 +1,9 @@
 from .loops import make_sde, make_ode, make_dde, make_sdde
 from .shtlc import make_shtdiff
-from .neural_mass import JRState, JRTheta, jr_dfun, jr_default_theta
+from .neural_mass import (
+        JRState, JRTheta, jr_dfun, jr_default_theta,
+        MPRState, MPRTheta, mpr_dfun, mpr_default_theta,
+        )
 from .regmap import make_region_mapping
 from .coupling import make_diff_cfun, make_linear_cfun
 from .connectome import make_conn_latent_mvnorm
