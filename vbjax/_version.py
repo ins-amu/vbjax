500162ad05b330e141df6332a8b6a055dd1cec54
marmaduke woodman <marmaduke.woodman@univ-amu.fr>
2023-11-13 18:26:24 +0100

    bump version

--- vbjax/_version.py
+++ vbjax/_version.py
@@ -1,4 +1,4 @@
-__version__ = 'v0.0.9'
+__version__ = 'v0.0.10'
 
 if __name__ == '__main__':
     print(__version__)
@@ -7,5 +7,7 @@ if __name__ == '__main__':
     print(cmd, args)
     if cmd == 'tag':
         os.system(f'git add vbjax/_version.py')
+        os.system(f'git commit -m "bump version"')
         os.system(f'git tag {__version__}')
+        os.system(f'git push -u origin main')
         os.system(f'git push -u origin {__version__}')
