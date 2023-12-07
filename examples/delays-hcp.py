c2ee39346194245dc81b43fdba4e1c648f7dd340
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2023-12-07 16:56:35 +0100

    finish 2nd scan for delays example

--- examples/delays-hcp.py
+++ examples/delays-hcp.py
@@ -50,10 +50,10 @@ def rgt0(rv, p):
     return jp.array([ r*(r>0), v ])
 
 # compile dfun w/ heun sdde for running a chunk
-_, run_chunk = vb.make_sdde(dt, max_lag, dfun, gfun=1e-3, unroll=4, adhoc=rgt0)
+_, run_chunk = vb.make_sdde(dt, max_lag, dfun, gfun=1e-3, unroll=10, adhoc=rgt0)
 
 # we'll run chunk of time this long
-chunk_len = 1000
+chunk_len = int(10 / dt) # 10 ms
 
 # buf should cover all delays + noise for time steps
 buf = jp.zeros((max_lag + chunk_len, 2, n_from))
@@ -75,39 +75,58 @@ buf, rv = run_chunk(buf, p)
 assert buf.shape[0] == (max_lag + chunk_len)
 assert rv.shape == (chunk_len, 2, n_from)
 
-# in a loop, careful with rng keys
-rng_keys = jax.random.split(jax.random.PRNGKey(42), 100)
-rs = [rv[::10,0]]
-
-def make_continue_chunk(chunk_len, max_lag, n_from):
+# jit the buffer updates
+# this kinda sucks but it's not model specific, so perfectly reusable
+def make_continue_chunk(run_chunk, chunk_len, max_lag, n_from, n_svar):
     # need to be compile time constants for dynamic_*
     i0 = chunk_len - 1
     i1 = max_lag + 1
-
     @jax.jit
     def continue_chunk(buf, p, key):
         get, set = jax.lax.dynamic_slice, jax.lax.dynamic_update_slice
-
         # buf = buf.at[:max_lag+1].set( buf[-(max_lag+1):] )
-        buf = set(buf, get(buf, (i0,0,0), (i1,2,n_from)), (0,0,0))
-
+        buf = set(buf, get(buf, (i0,0,0), (i1,n_svar,n_from)), (0,0,0))
         # buf = buf.at[max_lag+1:].set( vb.randn(chunk_len-1, 2, n_from, key=key) )
-        buf = set(buf, vb.randn(chunk_len-1, 2, n_from, key=key), (i1,0,0))
-
-        return run_chunk(buf, p)
+        buf = set(buf, vb.randn(chunk_len-1, n_svar, n_from, key=key), (i1,0,0))
+        buf, rv = run_chunk(buf, p)
+        return buf, rv
     return continue_chunk
-continue_chunk = make_continue_chunk(chunk_len, max_lag, n_from)
+continue_chunk = make_continue_chunk(run_chunk, chunk_len, max_lag, n_from, n_svar=buf.shape[1])
 
-# todo convert to scan
-for key in tqdm.tqdm(rng_keys):
-    # outside jax.jit these copy, but jit makes them inplace ops
+# setup time avg and bold monitors
+ta_buf, ta_step, ta_samp = vb.make_timeavg((2, n_from))
+ta_samp = vb.make_offline(ta_step, ta_samp)
+bold_buf, bold_step, bold_samp = vb.make_bold((2, n_from), dt, vb.bold_default_theta)
+bold_samp = vb.make_offline(bold_step, bold_samp)
+
+# now scan over rng keys (?)
+def chunk_ta_bold(bufs, key):
+    buf, ta_buf, bold_buf = bufs
     buf, rv = continue_chunk(buf, p, key)
-    rs.append(rv[::10,0])
-rs = jp.array(rs) # would happen automagically if loop done w/ scan
-print(rs.shape)
-rs = rs.reshape(-1, n_from) # reshape still required
+    ta_buf, ta = ta_samp(ta_buf, rv)
+    bold_buf, bold = bold_samp(bold_buf, rv)
+    return (buf, ta_buf, bold_buf), (ta, bold)
+
+@jax.jit
+def run_one_second(bufs, key):
+    keys = jax.random.split(key, 100) # 100 * 10 ms
+    return jax.lax.scan(chunk_ta_bold, bufs, keys)
+
+# pack buffers and run it one minute
+bufs = buf, ta_buf, bold_buf
+ta, bold = [], []
+keys = jax.random.split(jax.random.PRNGKey(42), 60)
+for i, key in enumerate(tqdm.tqdm(keys)):
+    bufs, (ta_i, bold_i) = run_one_second(bufs, key)
+    ta.append(ta_i)
+    bold.append(bold_i)
+ta = jp.array(ta).reshape((-1, 2, 70))
+bold = jp.array(bold).reshape((-1, 2, 70))
 
 # look at it
 import matplotlib.pyplot as pl
-pl.plot(rs, 'k', alpha=0.2)
+pl.subplot(221); pl.plot(ta[:, 0], 'k', alpha=0.2); pl.title('r t avg'); pl.grid(1)
+pl.subplot(222); pl.plot(bold[:, 0], 'b', alpha=0.2); pl.title('r bold'); pl.grid(1)
+pl.subplot(223); pl.plot(ta[:, 1], 'k', alpha=0.2); pl.title('r t avg'); pl.grid(1)
+pl.subplot(224); pl.plot(bold[:, 1], 'b', alpha=0.2); pl.title('r bold'); pl.grid(1)
 pl.show()
