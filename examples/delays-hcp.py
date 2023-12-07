1465109665120e622938e9f9e27c8f960cb2a1bd
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2023-12-07 13:11:23 +0100

    exa delay hcp jit continue chunk

--- examples/delays-hcp.py
+++ examples/delays-hcp.py
@@ -5,6 +5,7 @@ import numpy as np
 import jax
 import jax.numpy as jp
 import vbjax as vb
+import tqdm
 
 npz_fname = 'hcp-dk-dest-jub.npz'
 if not os.path.exists(npz_fname):
@@ -31,9 +32,9 @@ n_to, n_from = W.shape
 
 # setup aux vars for delays
 dt = 0.1
-v_c = 5.0 # m/s
-lags = jp.floor(L / v_c / dt).astype('i').T
-ix_lag_from = jp.tile(jp.r_[:n_from], (n_to, 1)).T
+v_c = 10.0 # m/s
+lags = jp.floor(L / v_c / dt).astype('i')
+ix_lag_from = jp.tile(jp.r_[:n_from], (n_to, 1))
 max_lag = lags.max() + 1
 Wt = jp.log(1+W.T[:,:,None]) # enable bcast for crv
 
@@ -41,14 +42,14 @@ Wt = jp.log(1+W.T[:,:,None]) # enable bcast for crv
 def dfun(buf, rv, t: int, p):
     # we could close over the vars or pass in like so:
     Wt, lags, ix_lag_from, mpr_theta, k = p
-    crv = (Wt * buf[t - lags, :, ix_lag_from]).sum(axis=0).T
+    crv = (Wt * buf[t - lags, :, ix_lag_from]).sum(axis=1).T
     return vb.mpr_dfun(rv, k*crv, mpr_theta)
 
 # compile dfun w/ heun sdde for running a chunk
 _, run_chunk = vb.make_sdde(dt, max_lag, dfun, gfun=1e-3, unroll=4)
 
 # we'll run chunk of time this long
-chunk_len = 42
+chunk_len = 1000
 
 # buf should cover all delays + noise for time steps
 buf = jp.zeros((max_lag + chunk_len, 2, n_from))
@@ -60,7 +61,7 @@ buf = buf.at[:max_lag+1].add( jp.r_[0.1,-2.0].reshape(2,1) )
 buf = buf.at[max_lag+1:].set( vb.randn(chunk_len-1, 2, n_from) )
 
 # pack parameters (could/should be dict / dataclass)
-k = 0.0
+k = 0.01
 p = Wt, lags, ix_lag_from, vb.mpr_default_theta, k
 
 # run it
@@ -71,19 +72,36 @@ assert buf.shape[0] == (max_lag + chunk_len)
 assert rv.shape == (chunk_len, 2, n_from)
 
 # in a loop, careful with rng keys
-rng_keys = jax.random.split(jax.random.PRNGKey(42), 10)
-rs = [rv[:,0]]
+rng_keys = jax.random.split(jax.random.PRNGKey(42), 100)
+rs = [rv[::10,0]]
+
+def make_continue_chunk(chunk_len, max_lag, n_from):
+    # need to be compile time constants for dynamic_*
+    i0 = chunk_len - 1
+    i1 = max_lag + 1
+
+    @jax.jit
+    def continue_chunk(buf, p, key):
+        get, set = jax.lax.dynamic_slice, jax.lax.dynamic_update_slice
+
+        # buf = buf.at[:max_lag+1].set( buf[-(max_lag+1):] )
+        buf = set(buf, get(buf, (i0,0,0), (i1,2,n_from)), (0,0,0))
+
+        # buf = buf.at[max_lag+1:].set( vb.randn(chunk_len-1, 2, n_from, key=key) )
+        buf = set(buf, vb.randn(chunk_len-1, 2, n_from, key=key), (i1,0,0))
+
+        return run_chunk(buf, p)
+    return continue_chunk
+continue_chunk = make_continue_chunk(chunk_len, max_lag, n_from)
+
 # todo convert to scan
-for key in rng_keys:
+for key in tqdm.tqdm(rng_keys):
     # outside jax.jit these copy, but jit makes them inplace ops
-    buf = buf.at[:max_lag+1].set( buf[-(max_lag+1):] )
-    buf = buf.at[max_lag+1:].set( vb.randn(chunk_len-1, 2, n_from, key=key) )
-    buf, rv = run_chunk(buf, p)
-    rs.append(rv[:,0])
+    buf, rv = continue_chunk(buf, p, key)
+    rs.append(rv[::10,0])
 rs = jp.array(rs) # would happen automagically if loop done w/ scan
 print(rs.shape)
 rs = rs.reshape(-1, n_from) # reshape still required
-
 
 # look at it
 import matplotlib.pyplot as pl
