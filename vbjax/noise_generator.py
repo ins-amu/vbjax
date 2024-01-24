05c352f6be84f7f7fb15bf762d669178f1b11b81
martin breyton <martinbreyton@martins-MacBook-Pro.local>
2024-01-24 12:39:17 +0100

    add documentation and remove useless args in PSDGenerator definition

--- vbjax/noise_generator.py
+++ vbjax/noise_generator.py
@@ -2,12 +2,37 @@ import jax.numpy as np
 import matplotlib.pyplot as plt
 import jax 
 
-
-def plot_spectrum(s):
-    f = np.fft.rfftfreq(len(s))
-    return plt.loglog(f, np.abs(np.fft.rfft(s)))[0]
-
 def noise_psd(shape, key, sigma=1, psd = lambda f: 1, **kwargs):
+        """Generate noise given desired power spectrum
+
+        Parameters
+        ==========
+        shape : tuple
+            (n_nodes, t_steps)
+        key : function
+            jax.random.PRNGKey()
+        sigma (optional kwarg): float
+            Standard deviation, defaults to 1
+        exponent (optional kwarg) : float
+            Spectral exponent for 1/f noise, defaults to 1
+            
+        Returns
+        =======
+        noise_stream : (n_nodes, t_steps) array
+            
+        Notes
+        =====
+        Example usage for white noise and 1/f noise
+
+        >>> import vbjax as vb, import jax
+        >>> key = jax.random.PRNGKey(seed)
+
+        >>> vbjax.noise_generator.white_noise((1,1000), key, sigma=2)
+        
+        >>> vbjax.noise_generator.spectral_exponent((84,10000), key, sigma=2, exponent=1.1)
+
+        """
+
         X_white = np.fft.rfft(jax.random.normal(key, shape)*sigma)
         S = psd(np.fft.rfftfreq(shape[1]), **kwargs)
         # Normalize S
@@ -15,7 +40,7 @@ def noise_psd(shape, key, sigma=1, psd = lambda f: 1, **kwargs):
         X_shaped = X_white * S
         return np.fft.irfft(X_shaped)
 
-def PSDGenerator(f, *args, **kwargs):
+def PSDGenerator(f):
     return lambda shape, *args, **kwargs,: noise_psd(shape, *args, **kwargs, psd=f)
 
 @PSDGenerator
