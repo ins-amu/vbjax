92b7cdd2ff538fd1b56d9925b3a9bfc051f17ac4
martin breyton <martinbreyton@martins-MacBook-Pro.local>
2024-03-19 12:55:15 +0100

    update structure

--- vbjax/noise_generator.py
+++ vbjax/noise_generator.py
@@ -2,7 +2,27 @@ import jax.numpy as np
 import matplotlib.pyplot as plt
 import jax 
 
-def noise_psd(shape, key, sigma=1, psd = lambda f: 1, **kwargs):
+
+def white_noise(f): 
+    return f
+
+def blue_noise(f):
+    return np.sqrt(f)
+
+def violet_noise(f):
+    return f
+
+def brownian_noise(f):
+    return 1/np.where(f == 0, float('inf'), f)
+
+def pink_noise(f):
+    return 1/np.where(f == 0, float('inf'), np.sqrt(f))
+
+def spectral_exponent(f, exponent=1):
+    return 1/np.where(f == 0, float('inf'), f**exponent)
+
+
+def make_noise_generator(psd = lambda f: 1):
         """Generate noise given desired power spectrum
 
         Parameters
@@ -27,44 +47,16 @@ def noise_psd(shape, key, sigma=1, psd = lambda f: 1, **kwargs):
         >>> import vbjax as vb, import jax
         >>> key = jax.random.PRNGKey(seed)
 
-        >>> vbjax.noise_generator.white_noise((1,1000), key, sigma=2)
-        
-        >>> vbjax.noise_generator.spectral_exponent((84,10000), key, sigma=2, exponent=1.1)
-
         """
+        def gen(key, shape, sigma=1, **kwargs):
+            # sigma = kwargs['sigma'] or 1
+            X_white = np.fft.rfft(jax.random.normal(key, shape)*sigma)
+            S = psd(np.fft.rfftfreq(shape[1]), **kwargs)
+            S = S / np.sqrt(np.mean(S**2))
+            X_shaped = X_white * S
+            return np.fft.irfft(X_shaped)
+        return gen
 
-        X_white = np.fft.rfft(jax.random.normal(key, shape)*sigma)
-        S = psd(np.fft.rfftfreq(shape[1]), **kwargs)
-        # Normalize S
-        S = S / np.sqrt(np.mean(S**2))
-        X_shaped = X_white * S
-        return np.fft.irfft(X_shaped)
 
-def PSDGenerator(f):
-    return lambda shape, *args, **kwargs,: noise_psd(shape, *args, **kwargs, psd=f)
-
-@PSDGenerator
-def white_noise(f):
-    return 1
-
-@PSDGenerator
-def blue_noise(f):
-    return np.sqrt(f)
-
-@PSDGenerator
-def violet_noise(f):
-    return f
-
-@PSDGenerator
-def brownian_noise(f):
-    return 1/np.where(f == 0, float('inf'), f)
-
-@PSDGenerator
-def pink_noise(f):
-    return 1/np.where(f == 0, float('inf'), np.sqrt(f))
-
-@PSDGenerator
-def spectral_exponent(f, exponent=1):
-    return 1/np.where(f == 0, float('inf'), f**exponent)
 
 
