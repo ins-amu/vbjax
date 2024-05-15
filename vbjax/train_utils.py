10624e7183d33bf23a5ca341b7a53b26b666a377
Martin Breyton <m.breyton@gmail.com>
2024-05-15 18:24:04 +0200

    jaxify train and eval functions

--- vbjax/train_utils.py
+++ vbjax/train_utils.py
@@ -2,38 +2,42 @@ import jax
 import jax.numpy as jnp
 from flax import linen as nn
 
-def log_likelihood(ms, logp, x):
+def log_likelihood_MADE(ms, logp, x, *args):
    u = jnp.exp(.5 * logp) * (x - ms)
    return -0.5 * (x.shape[0] * jnp.log(2 * jnp.pi) + jnp.sum(u ** 2 - logp, axis=1))
 
 
-def eval_MADE(made, params, batch, key, shape=None):
+def log_likelihood_MAF(u, logdet_dudx, *arg):
+    return -0.5 * u.shape[0] * jnp.log(2 * jnp.pi) - 0.5 * jnp.sum(u ** 2, axis=1) + logdet_dudx
+
+
+def eval_model(model, params, batch, key, likelihood_fn, shape=None):
   shape = shape if shape else batch.shape
-  def eval_model(made):
-    ms, logp = made(batch)
-    loss = -log_likelihood(ms, logp, batch).mean()
-    u_sample = made.gen(key, shape)
+  def eval_model(model):
+    output = model(batch)
+    loss = -likelihood_fn(*output, batch).mean()
+    u_sample = model.gen(key, shape)
     return loss, u_sample
-  return nn.apply(eval_model, made)({'params': params})
+  return nn.apply(eval_model, model)({'params': params})
 
 
-def train_step(state, batch):
+def train_step(state, batch, likelihood_fn):
   def loss_fn(params):
-    ms, logp = state.apply_fn(
+    output = state.apply_fn(
         {'params': params}, batch,
     )
-    loss = -log_likelihood(ms, logp, batch).mean()
+    loss = -likelihood_fn(*output, batch).mean()
     return loss
   grads = jax.grad(loss_fn)(state.params)
   return state.apply_gradients(grads=grads)
 
 
-def grad_func(state, batch):
+def grad_func(state, batch, likelihood_fn):
   def loss_fn(params):
-    ms, logp = state.apply_fn(
+    output = state.apply_fn(
         {'params': params}, batch,
     )
-    loss = -log_likelihood(ms, logp, batch).mean()
+    loss = -likelihood_fn(*output, batch).mean()
     return loss
   grads = jax.grad(loss_fn)(state.params)
   return grads
