d64bed0107e6e94d625aa5de0a578246412425cb
Martin Breyton <m.breyton@gmail.com>
2024-05-15 18:54:31 +0200

    add MAF implementation

--- vbjax/ml_models.py
+++ vbjax/ml_models.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 from flax import linen as nn
-from typing import Callable, Sequence
+from typing import Callable, Sequence, Optional
 from jax._src.prng import PRNGKeyArrayImpl
 import jax.random as random
 from vbjax.layers import MaskedMLP, OutputLayer, create_degrees, create_masks
@@ -11,27 +11,23 @@ class GaussianMADE(nn.Module):
     in_dim: int
     n_hiddens: Sequence[int]
     act_fn: Callable
+    input_order: str = 'sequential'
+    mode: str = 'sequential'
 
     def setup(self):
-        self.degrees = create_degrees(self.key, self.in_dim, self.n_hiddens, input_order='sequential', mode='sequential')
+        self.degrees = create_degrees(self.key, self.in_dim, self.n_hiddens, input_order=self.input_order, mode=self.mode)
         self.masks, self.out_mask = create_masks(self.degrees)
         self.mlp = MaskedMLP(self.n_hiddens, self.act_fn, self.masks)
         self.output_layer = OutputLayer(self.in_dim, self.out_mask)
 
     
     def __call__(self, inputs):
-        h = self.mlp(inputs) 
-        m, logp = self.output_layer(h) 
+        h = self.mlp(inputs)
+        m, logp = self.output_layer(h)
         return m, logp
 
 
     def gen(self, key, shape, u=None):
-        """
-        Generate samples from made. Requires as many evaluations as number of inputs.
-        :param n_samples: number of samples
-        :param u: random numbers to use in generating samples; if None, new random numbers are drawn
-        :return: samples
-        """
         x = jnp.zeros(shape)
         u = random.normal(key, shape) if u is None else u
 
@@ -40,4 +36,35 @@ class GaussianMADE(nn.Module):
             m, logp = self.output_layer(h) 
             idx = jnp.argwhere(self.degrees[0] == i)[0, 0]
             x = x.at[:, idx].set(m[:, idx] + jnp.exp(jnp.minimum(-0.5 * logp[:, idx], 10.0)) * u[:, idx])
+        return x
+
+
+class MAF(nn.Module):
+    key: PRNGKeyArrayImpl
+    in_dim: int
+    n_hiddens: Sequence[int]
+    act_fn: Callable
+    n_mades: int
+    input_order: Optional[Sequence] = None
+    mode: str = 'sequential'
+
+    def setup(self, input_order: Optional[Sequence] = None):
+        input_order = jnp.arange(1, self.in_dim+1) if input_order == None else input_order
+        self.mades = [GaussianMADE(random.split(self.key), self.in_dim, self.n_hiddens, self.act_fn, input_order=input_order[::((-1)**(i%2))], mode=self.mode) for i in range(self.n_mades)]
+
+    def __call__(self, inputs):
+        u = inputs
+        logdet_dudx = 0
+        for made in self.mades:
+            ms, logp = made(u)
+            u = jnp.exp(0.5 * logp) * (u - ms)
+            logdet_dudx += 0.5 * jnp.sum(logp, axis=1)
+        return u, logdet_dudx
+
+    def gen(self, key, shape, u=None):
+        x = random.normal(key, shape) if u is None else u
+
+        for made in self.mades[::-1]:
+            x = made.gen(key, shape, x)
+
         return x
